059e91ea797df22cb05feac5d68df5a104c0b839
Fecef <fecef.figueiredo@gmail.com>
2023-04-05 17:44:41 -0300

    docs: add install dep, run server and migrations.

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,3 +1,5 @@
 import app from "./app";
 
-app.listen(3001, () => console.log("Server running at: http://localhost:3001"));
+app.listen(process.env.PORT || 3001, () =>
+  console.log("Server running at: http://localhost:3001")
+);
