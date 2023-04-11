7aa79d2dcf718002c0ec6c91d86969d0c1df07c1
Germana-1 <germanagerriane@yahoo.com.br>
2023-04-11 11:45:09 -0300

    feat: filter with input

---
+++ front/src/components/FilterWithInputComponent/index.tsx
@@ -0,0 +1,36 @@
+import { Box, Heading, Flex, Input } from "@chakra-ui/react";
+import { Colors } from "../../styles";
+
+interface FilterProps {
+  titleFilter: string;
+}
+
+export const FilterWithInputComponent = ({ titleFilter }: FilterProps) => {
+  return (
+    <Box>
+      <Heading fontSize="28px" fontWeight="600" marginStart="30px">
+        {titleFilter}
+      </Heading>
+      <Flex
+        w="100%"
+        paddingStart="34px"
+        justifyContent="space-between"
+        gap="10px"
+        marginTop="18px"
+      >
+        <Input
+          maxW="142px"
+          bg={Colors.grey5}
+          placeholder="Mínimo"
+          borderRadius="0"
+        ></Input>
+        <Input
+          maxW="142px"
+          bg={Colors.grey5}
+          placeholder="Máximo"
+          borderRadius="0"
+        ></Input>
+      </Flex>
+    </Box>
+  );
+};
