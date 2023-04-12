af03da295872e18ae9c0ab5a3c55963b5f52d240
Germana-1 <germanagerriane@yahoo.com.br>
2023-04-12 17:33:30 -0300

    feat: mockedData

--- front/src/components/CardComponent/index.tsx
+++ front/src/components/CardComponent/index.tsx
@@ -10,8 +10,13 @@ import {
 } from "@chakra-ui/react";
 import { FontSizes } from "../../styles/fontSizes";
 import { Colors } from "../../styles/colors";
+import { IAnnouce } from "../ListCardComponent";
 
-export const CardComponent = () => {
+interface IProps {
+  annouce: IAnnouce;
+}
+
+export const CardComponent = ({ annouce }: IProps) => {
   return (
     <Box minW="312px" maxW="312px" margin="20px" position="relative">
       <Tag
@@ -32,30 +37,25 @@ export const CardComponent = () => {
         </TagLabel>
       </Tag>
       <Image
-        src="https://www.automaistv.com.br/wp-content/uploads/2022/01/fiat_mobi_like_52_edited-990x594.jpg"
+        src={annouce.banner}
         alt="Green double couch with wooden legs"
         border={`2px solid ${Colors.grey10}`}
         _hover={{ cursor: "pointer", border: `2px solid ${Colors.brand1}` }}
       />
       <Flex display="flex" flexDirection="column" gap="16px" marginTop="16px">
-        <Heading size="16px">Mobi</Heading>
+        <Heading size="16px">{`${annouce.brand} - ${annouce.model}`}</Heading>
         <Text
           fontSize="14px"
           fontWeight="400"
           color={Colors.grey2}
           lineHeight="24px"
         >
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem...
+          {annouce.description}
         </Text>
         <Flex gap="10px" alignItems="center">
-          <Avatar
-            src="https://www.automaistv.com.br/wp-content/uploads/2022/01/fiat_mobi_like_52_edited-990x594.jpg"
-            w="32px"
-            h="32px"
-          />
+          <Avatar src={annouce.user.avatar} w="32px" h="32px" />
           <Text fontSize="14px" fontWeight="500" color={Colors.grey2}>
-            Samuel Leão
+            {annouce.user.name}
           </Text>
         </Flex>
         <Flex justifyContent="space-between">
@@ -67,7 +67,7 @@ export const CardComponent = () => {
               fontWeight="500"
               padding="4px"
             >
-              0 KM
+              {`${annouce.mileage} KM`}
             </Text>
             <Text
               bg={Colors.brand4}
@@ -76,11 +76,11 @@ export const CardComponent = () => {
               fontWeight="500"
               padding="4px"
             >
-              2019
+              {annouce.year}
             </Text>
           </Flex>
           <Text fontSize="16px" fontWeight="500" color={Colors.grey1}>
-            R$ 00.000,00
+            {`R$${annouce.price}`}
           </Text>
         </Flex>
       </Flex>
--- front/src/components/ListCardComponent/index.tsx
+++ front/src/components/ListCardComponent/index.tsx
@@ -1,22 +1,47 @@
 import { Flex, useMediaQuery } from "@chakra-ui/react";
 
 import { CardComponent } from "../../components/CardComponent";
+import { useEffect, useState } from "react";
+import { mockedData } from "../../_mockedData";
+
+export interface IAnnouce {
+  id: string;
+  brand: string;
+  model: string;
+  year: number;
+  mileage: number;
+  color: string;
+  price: number;
+  priceFipe: number;
+  fuelType: string;
+  description: string;
+  banner: string;
+  isGoodBuy: boolean;
+  user: {
+    name: string;
+    avatar: string;
+  };
+}
 
 export const ListCardComponent = () => {
   const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const [annouceList, setAnnouceList] = useState<IAnnouce[]>([]);
+
+  useEffect(() => {
+    setAnnouceList(mockedData);
+  }, []);
+
   return (
     <Flex
+      as="ul"
       flexWrap={isMobile ? "nowrap" : "wrap"}
       justifyContent="flex-start"
       overflowX="auto"
       maxW={isMobile ? "100%" : "85%"}
     >
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
+      {annouceList.map((annouce) => (
+        <CardComponent annouce={annouce} key={annouce.id} />
+      ))}
     </Flex>
   );
 };
