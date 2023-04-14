1e91cee542e75b111a141a0125a0020f062533ce
Fecef <fecef.figueiredo@gmail.com>
2023-04-14 12:58:13 -0300

    style: change gallery style.

--- front/src/components/AnnounceDetailGalleryComponent/index.tsx
+++ front/src/components/AnnounceDetailGalleryComponent/index.tsx
@@ -1,13 +1,41 @@
-import { Box, Flex, Image, useDisclosure } from "@chakra-ui/react";
+import { Flex, useDisclosure } from "@chakra-ui/react";
 
 import car from "../../assets/unsplash_ZVgPUWC9Mjs.png";
 import { Colors } from "../../styles/colors";
 import { TextH6 } from "../FontComponents";
 import { ModalCarGalleryComponent } from "../ModalCarGalleryComponent";
+import { GalleryCarousel } from "../GalleryCarouselComponent";
 
 export const AnnounceDetailGalleryComponent = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cars = [1, 2, 3, 4, 5, 6];
+
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
 
   return (
     <>
@@ -21,27 +49,7 @@ export const AnnounceDetailGalleryComponent = () => {
         <TextH6 fontWeight="600">Fotos</TextH6>
 
         <Flex gap={"20px"} wrap={"wrap"} justifyContent={"center"}>
-          {cars.map((el, i) => {
-            return (
-              <Box
-                backgroundColor={Colors.grey7}
-                h={"80px"}
-                w={"80px"}
-                borderRadius={"4px"}
-                cursor={"pointer"}
-                onClick={onOpen}
-                key={i}
-              >
-                <Image
-                  src={car}
-                  w={"inherit"}
-                  h={"inherit"}
-                  objectFit={"contain"}
-                  onClick={onOpen}
-                />
-              </Box>
-            );
-          })}
+          <GalleryCarousel items={images} />
         </Flex>
       </Flex>
 
