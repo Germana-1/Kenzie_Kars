13488c9688578966774ce1feae0f93f337efbbd3
Fecef <fecef.figueiredo@gmail.com>
2023-04-16 19:06:45 -0300

    feat: add profile view page.

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import { HomePage } from "../pages/HomePage";
 import { ProductPage } from "../pages/ProductPage";
+import { ProfileViewPage } from "../pages/ProfileViewPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,6 +10,7 @@ export const RoutesMain = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/detail" element={<ProductPage />} />
+        <Route path="/profile" element={<ProfileViewPage />} />
       </Routes>
     </>
   );
