8d26c0492e69cdcc80c64f512d19e3e4f50c2d84
Fecef <fecef.figueiredo@gmail.com>
2023-04-16 19:54:28 -0300

    feat: add announcements list.

---
+++ front/src/components/ProfileViewAnnouncerInfoComponent/index.tsx
@@ -0,0 +1,30 @@
+import { Avatar, Flex } from "@chakra-ui/react";
+
+import { TextB1, TextH6 } from "../FontComponents";
+import { ButtonBrand4 } from "../ButtomComponents";
+import { Colors } from "../../styles/colors";
+
+export const ProfileViewAnnouncerInfoComponent = () => {
+  return (
+    <Flex
+      direction={"column"}
+      gap={"20px"}
+      p={"28px 44px"}
+      borderRadius={"4px"}
+      backgroundColor={Colors.grey10}
+    >
+      <Avatar size={"xl"} />
+
+      <Flex gap={"10px"} alignItems={"center"}>
+        <TextH6 fontWeight={"600"}>Samuel Leão</TextH6>
+        <ButtonBrand4 size={"sm"}>Anunciante</ButtonBrand4>
+      </Flex>
+
+      <TextB1 fontWeight={"400"} color={Colors.grey2}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s
+      </TextB1>
+    </Flex>
+  );
+};
--- front/src/pages/ProfileViewPage/index.tsx
+++ front/src/pages/ProfileViewPage/index.tsx
@@ -1,5 +1,34 @@
+import { Avatar, Box, Container, Flex } from "@chakra-ui/react";
+
+import { FooterComponent } from "../../components/FooterComponent";
+import { HeaderComponent } from "../../components/HeaderComponent";
+import { ListCardComponent } from "../../components/ListCardComponent";
+import { TextB1, TextH5, TextH6 } from "../../components/FontComponents";
+import { ButtonBrand4 } from "../../components/ButtomComponents";
+import { Colors } from "../../styles/colors";
+import { ProfileViewAnnouncerInfoComponent } from "../../components/ProfileViewAnnouncerInfoComponent";
+
 export const ProfileViewPage = () => {
-    return (
-        <h1>Profile</h1>
-    )   
-}
+  return (
+    <>
+      <Flex
+        flexDirection={"column"}
+        bgGradient={
+          "linear(0deg, rgba(233,236,239,1) 90%, rgba(69,41,230,1) 90%)"
+        }
+      >
+        <HeaderComponent />
+
+        <Container maxW="1200px" pt={"130px"}>
+          <ProfileViewAnnouncerInfoComponent />
+
+          <TextH5 fontWeight={"600"}>Anúncios</TextH5>
+
+          <ListCardComponent />
+        </Container>
+
+        <FooterComponent />
+      </Flex>
+    </>
+  );
+};
