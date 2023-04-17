70cd8cf0d280c61207ec396b3135c0931be46838
Alfredo <alfredofontinele08@gmail.com>
2023-04-17 15:34:40 -0300

    feat: add register page

--- front/src/pages/RegisterPage/index.tsx
+++ front/src/pages/RegisterPage/index.tsx
@@ -1,14 +1,13 @@
-import {
-    Button,
-    Flex,
-    Input,
-    FormLabelProps,
-    Container,
-    Text,
-} from "@chakra-ui/react";
+import { Flex, FormControl, Text } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { useRef, useState } from "react";
+
 import { HeaderComponent } from "./../../components/HeaderComponent/index";
 import { InputFormComponent } from "./../../components/InputFormComponent/index";
 import { ButtonBrand1, ButtonGray10 } from "../../components/ButtomComponents";
+import { registerUserSchema } from "../../schemas";
+import { ErrorComponent } from "./../../components/ErrorComponent/index";
 
 const formStyle = {
     width: "100%",
@@ -18,6 +17,52 @@ const formStyle = {
 };
 
 export const RegisterPage = () => {
+    const [isError, setIsError] = useState<boolean>(true);
+
+    const [optionIsBuyer, setOptionIsBuyer] = useState<boolean>(false);
+    const [optionIsAdvertiser, setOptionIsAdvertiser] =
+        useState<boolean>(false);
+
+    const setBuyerOption = () => {
+        setIsError(false);
+        if (optionIsBuyer) {
+            setOptionIsBuyer(false);
+            setOptionIsAdvertiser(true);
+            return false;
+        }
+        setOptionIsBuyer(true);
+        setOptionIsAdvertiser(false);
+        return true;
+    };
+
+    const setAdvertiserOption = () => {
+        setIsError(false);
+        if (optionIsAdvertiser) {
+            setOptionIsAdvertiser(false);
+            setOptionIsBuyer(true);
+            return false;
+        }
+        setOptionIsAdvertiser(true);
+        setOptionIsBuyer(false);
+        return true;
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(registerUserSchema),
+    });
+
+    const registerUserForm = (data: any) => {
+        const dataUpdate = {
+            ...data,
+            account_type: optionIsBuyer ? "buyer" : "advertiser",
+        };
+        console.log(dataUpdate);
+    };
+
     return (
         <>
             <HeaderComponent />
@@ -32,7 +77,10 @@ export const RegisterPage = () => {
                     <Text fontSize={24} fontWeight={500}>
                         Cadastro
                     </Text>
-                    <form style={{ ...formStyle, flexDirection: "column" }}>
+                    <form
+                        onSubmit={handleSubmit(registerUserForm)}
+                        style={{ ...formStyle, flexDirection: "column" }}
+                    >
                         <Flex flexDir={"column"} gap={5}>
                             <Text fontSize={14} fontWeight={500}>
                                 Informações Pessoais
@@ -40,30 +88,48 @@ export const RegisterPage = () => {
                             <InputFormComponent
                                 labelText={"Nome"}
                                 placeholder={"Ex: Samuel Leão"}
+                                name="name"
+                                register={register}
+                                errors={errors}
                             />
                             <InputFormComponent
                                 labelText={"Email"}
                                 placeholder={"Ex: samuel@kenzie.com.br"}
+                                register={register}
+                                errors={errors}
+                                name="email"
                             />
                             <InputFormComponent
                                 mask={"999.999.999-99"}
                                 labelText={"CPF"}
                                 placeholder={"000.000.000-00"}
+                                register={register}
+                                errors={errors}
+                                name="cpf"
                             />
                             <InputFormComponent
                                 mask={"(99) 99999-9999"}
                                 labelText={"Celular"}
                                 placeholder={"(DDD) 90000-0000"}
+                                register={register}
+                                errors={errors}
+                                name="phone"
                             />
                             <InputFormComponent
                                 mask={"99/99/9999"}
                                 labelText={"Data de Nascimento"}
                                 placeholder={"00/00/0000"}
+                                register={register}
+                                errors={errors}
+                                name="birthdate"
                             />
                             <InputFormComponent
                                 hasTextArea={true}
                                 labelText={"Descrição"}
                                 placeholderTextArea={"Digitar descrição"}
+                                register={register}
+                                errors={errors}
+                                name="description"
                             />
                         </Flex>
                         <Flex flexDir={"column"} gap={5}>
@@ -74,52 +140,99 @@ export const RegisterPage = () => {
                                 mask={"99999.999"}
                                 labelText={"CEP"}
                                 placeholder={"00000.000"}
+                                register={register}
+                                errors={errors}
+                                name="cep"
                             />
                             <Flex gap={3}>
-                                <InputFormComponent
-                                    labelText={"Estado"}
-                                    placeholder={"Digitar Estado"}
-                                />
-                                <InputFormComponent
-                                    labelText={"Cidade"}
-                                    placeholder={"Digitar Cidade"}
-                                />
+                                <Flex flexDir={"column"} gap={2}>
+                                    <InputFormComponent
+                                        labelText={"Estado"}
+                                        placeholder={"Digitar Estado"}
+                                        register={register}
+                                        errors={errors}
+                                        name="state"
+                                    />
+                                </Flex>
+                                <Flex flexDir={"column"} gap={2}>
+                                    <InputFormComponent
+                                        labelText={"Cidade"}
+                                        placeholder={"Digitar Cidade"}
+                                        register={register}
+                                        errors={errors}
+                                        name="city"
+                                    />
+                                </Flex>
                             </Flex>
                             <InputFormComponent
                                 labelText={"Rua"}
                                 placeholder={"Digitar Rua"}
+                                register={register}
+                                errors={errors}
+                                name="street"
                             />
                             <Flex gap={3}>
-                                <InputFormComponent
-                                    labelText={"Número"}
-                                    type="number"
-                                    placeholder={"Digitar Numero"}
-                                />
-                                <InputFormComponent
-                                    labelText={"Complemento"}
-                                    placeholder={"Ex: apart 307"}
-                                />
+                                <Flex flexDir={"column"} gap={2}>
+                                    <InputFormComponent
+                                        labelText={"Número"}
+                                        type="number"
+                                        placeholder={"Digitar Numero"}
+                                        register={register}
+                                        errors={errors}
+                                        name="house_number"
+                                    />
+                                </Flex>
+                                <Flex flexDir={"column"} gap={2}>
+                                    <InputFormComponent
+                                        labelText={"Complemento"}
+                                        placeholder={"Ex: apart 307"}
+                                        register={register}
+                                        errors={errors}
+                                        name="complement"
+                                    />
+                                </Flex>
                             </Flex>
                         </Flex>
                         <Flex flexDir={"column"} gap={5}>
                             <Text fontSize={14} fontWeight={500}>
                                 Tipos de conta
                             </Text>
-                            <Flex gap={3}>
-                                <ButtonBrand1 w={"full"}>
-                                    Comprador
-                                </ButtonBrand1>
-                                <ButtonGray10 w={"full"}>
-                                    Anunciante
-                                </ButtonGray10>
-                            </Flex>
+                            <FormControl isRequired isInvalid={isError}>
+                                <Flex gap={3}>
+                                    <ButtonBrand1
+                                        w={"full"}
+                                        isDisabled={optionIsBuyer}
+                                        onClick={() => setBuyerOption()}
+                                    >
+                                        Comprador
+                                    </ButtonBrand1>
+                                    <ButtonGray10
+                                        w={"full"}
+                                        isDisabled={optionIsAdvertiser}
+                                        onClick={() => setAdvertiserOption()}
+                                    >
+                                        Anunciante
+                                    </ButtonGray10>
+                                </Flex>
+                                {!!isError && (
+                                    <ErrorComponent
+                                        text={"Escolha uma opção."}
+                                    />
+                                )}
+                            </FormControl>
                             <InputFormComponent
                                 labelText={"Senha"}
                                 placeholder={"Digitar senha"}
+                                register={register}
+                                errors={errors}
+                                name="password"
                             />
                             <InputFormComponent
                                 labelText={"Confirmar senha"}
                                 placeholder={"Digitar senha"}
+                                register={register}
+                                errors={errors}
+                                name="confirm_password"
                             />
                         </Flex>
                         <ButtonBrand1 type="submit">
