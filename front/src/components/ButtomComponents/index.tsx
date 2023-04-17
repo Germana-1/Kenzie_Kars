67f54a42eecbf154897d74dc37dc05c38db80e54
Alfredo <alfredofontinele08@gmail.com>
2023-04-17 16:27:52 -0300

    refac: refactor all props buttons

--- front/src/components/ButtomComponents/index.tsx
+++ front/src/components/ButtomComponents/index.tsx
@@ -2,10 +2,9 @@ import { Button, ButtonProps } from "@chakra-ui/react";
 
 import { Colors } from "../../styles/colors";
 
-export const ButtonGray0 = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonGray0 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.grey0}
             color={Colors.white}
@@ -14,15 +13,14 @@ export const ButtonGray0 = ({ children, size, ...rest }: ButtonProps) => {
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonGray6 = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonGray6 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.grey6}
             color={Colors.grey2}
@@ -31,37 +29,27 @@ export const ButtonGray6 = ({ children, size, ...rest }: ButtonProps) => {
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonGray5 = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonGray5 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.grey5}
             color={Colors.grey2}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonBrand1 = ({
-    children,
-    size,
-    width,
-    isDisabled,
-    onClick,
-    ...rest
-}: ButtonProps) => {
+export const ButtonBrand1 = ({ isDisabled, onClick, ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
-            width={width}
             isDisabled={isDisabled}
             fontWeight={"600"}
             backgroundColor={isDisabled ? Colors.brand3 : Colors.brand1}
@@ -72,38 +60,30 @@ export const ButtonBrand1 = ({
             onClick={onClick}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonBrand4 = ({
-    children,
-    size,
-    cursor,
-    ...rest
-}: ButtonProps) => {
+export const ButtonBrand4 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.brand4}
             color={Colors.brand1}
-            cursor={cursor}
             _hover={{
                 backgroundColor: Colors.brand4,
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonGray10 = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonGray10 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.grey10}
             color={Colors.grey0}
@@ -113,19 +93,14 @@ export const ButtonGray10 = ({ children, size, ...rest }: ButtonProps) => {
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonGray10OutlineG4 = ({
-    children,
-    size,
-    ...rest
-}: ButtonProps) => {
+export const ButtonGray10OutlineG4 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.white}
             color={Colors.grey0}
@@ -137,19 +112,14 @@ export const ButtonGray10OutlineG4 = ({
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonBrand1OutlineBrand1 = ({
-    children,
-    size,
-    ...rest
-}: ButtonProps) => {
+export const ButtonBrand1OutlineBrand1 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.grey10}
             color={Colors.brand1}
@@ -159,15 +129,14 @@ export const ButtonBrand1OutlineBrand1 = ({
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonTransparent = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonTransparent = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor="transparent"
             color={Colors.grey0}
@@ -176,53 +145,46 @@ export const ButtonTransparent = ({ children, size, ...rest }: ButtonProps) => {
             }}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonAlert3 = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonAlert3 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.alert3}
             color={Colors.alert1}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonSuccess3 = ({ children, size, ...rest }: ButtonProps) => {
+export const ButtonSuccess3 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.success3}
             color={Colors.success1}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
 
-export const ButtonBrand3Disable = ({
-    children,
-    size,
-    ...rest
-}: ButtonProps) => {
+export const ButtonBrand3Disable = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            size={size}
             fontWeight={"600"}
             backgroundColor={Colors.brand3}
             color={Colors.brand4}
             {...rest}
         >
-            {children}
+            {rest.children}
         </Button>
     );
 };
