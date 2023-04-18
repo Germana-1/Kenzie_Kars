4dda3f7c7e3c14730a093b938554f11ef40265bb
Germana-1 <germanagerriane@yahoo.com.br>
2023-04-18 11:06:03 -0300

    refac: add stripUnknown and abortEarly

--- back/src/middlewares/validations/validateSchema.ts
+++ back/src/middlewares/validations/validateSchema.ts
@@ -2,14 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import * as yup from "yup";
 
 export const validateSchema =
-    (schema: yup.BaseSchema) =>
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            await schema.validate(req.body);
-            return next();
-        } catch (err: any) {
-            return res.status(400).json({
-                message: err.message,
-            });
-        }
-    };
+  (schema: yup.BaseSchema) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.validate(req.body, {
+        stripUnknown: true,
+        abortEarly: false,
+      });
+      return next();
+    } catch (err: any) {
+      return res.status(400).json({
+        message: err.message,
+      });
+    }
+  };
