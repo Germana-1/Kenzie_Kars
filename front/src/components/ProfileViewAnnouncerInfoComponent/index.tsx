66f914d95963c2873252a4582ec51f7b4ba569b5
Fecef <fecef.figueiredo@gmail.com>
2023-04-18 13:34:25 -0300

    feat: add announcement create modal.

--- front/src/components/ProfileViewAnnouncerInfoComponent/index.tsx
+++ front/src/components/ProfileViewAnnouncerInfoComponent/index.tsx
@@ -1,36 +1,42 @@
-import { Avatar, Flex } from "@chakra-ui/react";
+import { Avatar, Flex, useDisclosure } from "@chakra-ui/react";
 
 import { TextB1, TextH6 } from "../TextComponents";
 import { ButtonBrand1OutlineBrand1, ButtonBrand4 } from "../ButtomComponents";
 import { Colors } from "../../styles/colors";
+import { ModalRegisterAnnoucement } from "../Modal";
 
 export const ProfileViewAnnouncerInfoComponent = () => {
-  return (
-    <Flex
-      direction={"column"}
-      gap={"20px"}
-      p={"28px 44px"}
-      borderRadius={"4px"}
-      backgroundColor={Colors.grey10}
-    >
-      <Avatar size={"xl"} />
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-      <Flex gap={"10px"} alignItems={"center"}>
-        <TextH6 fontWeight={"600"}>Samuel Leão</TextH6>
-        <ButtonBrand4 size={"sm"} cursor={"default"}>
-          Anunciante
-        </ButtonBrand4>
+  return (
+    <>
+      <Flex
+        direction={"column"}
+        gap={"20px"}
+        p={"28px 44px"}
+        borderRadius={"4px"}
+        backgroundColor={Colors.grey10}
+      >
+        <Avatar size={"xl"} />
+        <Flex gap={"10px"} alignItems={"center"}>
+          <TextH6 fontWeight={"600"}>Samuel Leão</TextH6>
+          <ButtonBrand4 size={"sm"} cursor={"default"}>
+            Anunciante
+          </ButtonBrand4>
+        </Flex>
+
+        <TextB1 fontWeight={"400"} color={Colors.grey2}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </TextB1>
+
+        <ButtonBrand1OutlineBrand1 alignSelf={"flex-start"} onClick={onOpen}>
+          Criar anúncio
+        </ButtonBrand1OutlineBrand1>
       </Flex>
 
-      <TextB1 fontWeight={"400"} color={Colors.grey2}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s
-      </TextB1>
-
-      <ButtonBrand1OutlineBrand1 alignSelf={"flex-start"}>
-        Criar anúncio
-      </ButtonBrand1OutlineBrand1>
-    </Flex>
+      <ModalRegisterAnnoucement isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
