6b46853440a23c737473a2439c57fb3aecc490be
Fecef <fecef.figueiredo@gmail.com>
2023-04-18 13:36:19 -0300

    style: remove fontweight quotes.

--- front/src/components/ButtomComponents/index.tsx
+++ front/src/components/ButtomComponents/index.tsx
@@ -5,7 +5,7 @@ import { Colors } from "../../styles/colors";
 export const ButtonGray0 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.grey0}
             color={Colors.white}
             _hover={{
@@ -21,7 +21,7 @@ export const ButtonGray0 = ({ ...rest }: ButtonProps) => {
 export const ButtonGray6 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.grey6}
             color={Colors.grey2}
             _hover={{
@@ -37,7 +37,7 @@ export const ButtonGray6 = ({ ...rest }: ButtonProps) => {
 export const ButtonGray5 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.grey5}
             color={Colors.grey2}
             {...rest}
@@ -51,7 +51,7 @@ export const ButtonBrand1 = ({ isDisabled, onClick, ...rest }: ButtonProps) => {
     return (
         <Button
             isDisabled={isDisabled}
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={isDisabled ? Colors.brand3 : Colors.brand1}
             color={Colors.white}
             _hover={{
@@ -68,7 +68,7 @@ export const ButtonBrand1 = ({ isDisabled, onClick, ...rest }: ButtonProps) => {
 export const ButtonBrand4 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.brand4}
             color={Colors.brand1}
             _hover={{
@@ -84,7 +84,7 @@ export const ButtonBrand4 = ({ ...rest }: ButtonProps) => {
 export const ButtonGray10 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.grey10}
             color={Colors.grey0}
             outline={`1.5px solid ${Colors.grey0}`}
@@ -101,7 +101,7 @@ export const ButtonGray10 = ({ ...rest }: ButtonProps) => {
 export const ButtonGray10OutlineG4 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.white}
             color={Colors.grey0}
             outline={Colors.grey4}
@@ -120,7 +120,7 @@ export const ButtonGray10OutlineG4 = ({ ...rest }: ButtonProps) => {
 export const ButtonBrand1OutlineBrand1 = ({ ...rest }: ButtonProps) => {
   return (
     <Button
-      fontWeight={"600"}
+      fontWeight={600}
       backgroundColor={Colors.grey10}
       color={Colors.brand1}
       border={`1.5px solid ${Colors.brand1}`}
@@ -137,7 +137,7 @@ export const ButtonBrand1OutlineBrand1 = ({ ...rest }: ButtonProps) => {
 export const ButtonTransparent = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor="transparent"
             color={Colors.grey0}
             _hover={{
@@ -153,7 +153,7 @@ export const ButtonTransparent = ({ ...rest }: ButtonProps) => {
 export const ButtonAlert3 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.alert3}
             color={Colors.alert1}
             {...rest}
@@ -166,7 +166,7 @@ export const ButtonAlert3 = ({ ...rest }: ButtonProps) => {
 export const ButtonSuccess3 = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.success3}
             color={Colors.success1}
             {...rest}
@@ -179,7 +179,7 @@ export const ButtonSuccess3 = ({ ...rest }: ButtonProps) => {
 export const ButtonBrand3Disable = ({ ...rest }: ButtonProps) => {
     return (
         <Button
-            fontWeight={"600"}
+            fontWeight={600}
             backgroundColor={Colors.brand3}
             color={Colors.brand4}
             {...rest}
