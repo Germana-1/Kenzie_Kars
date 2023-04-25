d03d42a7106702c295a83a6ce7b476ad12f4ea50
Fecef <fecef.figueiredo@gmail.com>
2023-04-25 15:01:22 -0300

    style: remove placeholder.

--- front/src/components/ModalComponents/ModalRegisterAnnouncementComponent/index.tsx
+++ front/src/components/ModalComponents/ModalRegisterAnnouncementComponent/index.tsx
@@ -140,9 +140,6 @@ export const ModalRegisterAnnoucement = ({ isOpen, onClose }: IModal) => {
             <InputFormComponent
               labelText={"Descrição"}
               hasTextArea
-              placeholderTextArea={
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-              }
               name="description"
               register={register}
               errors={errors}
