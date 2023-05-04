1b85923f4c33a835f78caf3a19425e5e4873ed85
Alfredo <alfredofontinele08@gmail.com>
2023-05-04 18:06:02 -0300

    refac: update modal error component

--- front/src/components/ModalComponents/ModalErrorComponent/index.tsx
+++ front/src/components/ModalComponents/ModalErrorComponent/index.tsx
@@ -3,19 +3,20 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
   ModalProps,
 } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
-import { useState, useContext, useEffect } from "react";
-import {
-  ButtonBrand1,
-} from "../../ButtomComponents";
+import React from "react";
 import { TextB2, TextH7 } from "../../TextComponents";
 
-export const ModalError = ({ isOpen, onClose }: ModalProps) => {
+interface IModalError extends ModalProps {
+  textHeader: string
+  textBody: string
+  children: React.ReactNode
+}
+
+export const ModalError = ({ textHeader, textBody, isOpen, onClose, children }: IModalError) => {
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -27,13 +28,14 @@ export const ModalError = ({ isOpen, onClose }: ModalProps) => {
           zIndex="10000"
         >
           <ModalHeader>
-            <TextH7>Erro!</TextH7>
+            <TextH7>{textHeader}</TextH7>
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody display="flex" flexDirection="column" gap="20px" p={5}>
             <TextB2>
-              CEP Inválido
+              {textBody}
             </TextB2>
+            {children}
           </ModalBody>
         </ModalContent>
       </Modal>
