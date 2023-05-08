a302eb053a88ad8d3753972dc39a2765717511d4
Alfredo <alfredofontinele08@gmail.com>
2023-05-08 17:26:58 -0300

    feat: add ModalHomeFiltersComponent

--- front/src/pages/HomePage/index.tsx
+++ front/src/pages/HomePage/index.tsx
@@ -1,14 +1,14 @@
 import {
-  Box,
-  Flex,
-  Modal,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  Show,
-  useDisclosure,
-  useMediaQuery,
+    Box,
+    Flex,
+    Modal,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay,
+    Show,
+    useDisclosure,
+    useMediaQuery,
 } from "@chakra-ui/react";
 
 import { HeaderComponent } from "../../components/HeaderComponent";
@@ -22,91 +22,63 @@ import { ModalEditAd } from "../../components/ModalComponents/ModalEditAdCompone
 import { ModalDeleteAd } from "../../components/ModalComponents/ModalDeleteAdComponent";
 import { useContext } from "react";
 import { AnnouncementContext } from "../../contexts/announcementContext";
+import { ModalHomeFIltersComponent } from "../../components/ModalComponents/ModalHomeFiltersComponent";
 
 export const HomePage = () => {
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
-  const { isOpen, onOpen, onClose } = useDisclosure();
+    const [isMobile] = useMediaQuery("(max-width: 768px)");
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const {
-    editAdModalOpen,
-    setEditAdModalOpen,
-    deleteAdModalOpen,
-    setDeleteAdModalOpen
-  } = useContext(AnnouncementContext);
+    const {
+        editAdModalOpen,
+        setEditAdModalOpen,
+        deleteAdModalOpen,
+        setDeleteAdModalOpen,
+    } = useContext(AnnouncementContext);
 
-  return (
-    <Flex flexDirection={"column"}>
-      <HeaderComponent />
-      <BannerComponent />
-      <Flex
-        w="96%"
-        margin={"70px auto 0"}
-        justifyContent="space-between"
-        alignItems={isMobile ? "center" : "start"}
-        flexDir={isMobile ? "column" : "row"}
-      >
-        <Box minW={"400px"}>
-          <Show above="768px">
-            <ListFiltersComponent />
-          </Show>
-        </Box>
-
-        <Flex w={"1325px"} maxW={"100%"} justifyContent={"center"}>
-          <ListCardComponent filterActive hideTag />
-        </Flex>
-
-        <Show below="768px">
-          <Flex margin="90px" alignItems="center">
-            <ButtonBrand1 width="279px" onClick={onOpen}>
-              Filtros
-            </ButtonBrand1>
-          </Flex>
-
-          <Flex>
-            <Modal onClose={onClose} isOpen={isOpen}>
-              <ModalOverlay />
-              <ModalContent
-                display="flex"
-                alignItems="center"
-                marginTop="80px"
-                borderRadius="0"
-              >
-                <ModalHeader
-                  fontSize={FontSizes.heading7}
-                  fontFamily={"Lexend"}
-                  fontWeight="500"
-                  alignSelf="start"
-                  p={"15px"}
+    return (
+        <Flex flexDirection={"column"}>
+            <HeaderComponent />
+            <BannerComponent />
+            <Flex
+                w="full"
+                m={"70px auto 0"}
+                alignItems={isMobile ? "center" : "start"}
+                justifyContent={"space-between"}
+                flexDir={isMobile ? "column" : "row"}
+                px={10}
+                gap={50}
+            >
+                <Show above="1000px">
+                    <ListFiltersComponent />
+                </Show>
+                <Flex
+                    flexDir={"column"}
+                    justifyContent={"center"}
+                    alignItems={"center"}
                 >
-                  Filtro
-                </ModalHeader>
-                <ModalCloseButton />
-
-                <ListFiltersComponent />
-
-                <Flex marginTop="42px" marginBottom="32px">
-                  <ButtonBrand1 width="279px" onClick={onClose}>
-                    Ver anúncios
-                  </ButtonBrand1>
+                    <ListCardComponent filterActive hideTag />
+                    <Show below="1000px">
+                        <Flex margin="90px" alignItems="center">
+                            <ButtonBrand1 width="279px" onClick={onOpen}>
+                                Filtros
+                            </ButtonBrand1>
+                        </Flex>
+                    </Show>
                 </Flex>
-              </ModalContent>
-            </Modal>
-          </Flex>
-        </Show>
-      </Flex>
-      <FooterComponent />
-
-      <ModalEditAd
-        isOpen={editAdModalOpen}
-        onClose={() => setEditAdModalOpen(false)}
-        children={undefined}
-      />
-      <ModalDeleteAd
-        isOpen={deleteAdModalOpen}
-        onClose={() => setDeleteAdModalOpen(false)}
-        children={undefined}
-      />
-
-    </Flex>
-  );
+            </Flex>
+            <FooterComponent />
+            <ModalHomeFIltersComponent 
+              onClose={onClose} 
+              isOpen={isOpen} 
+            />
+            <ModalEditAd
+                isOpen={editAdModalOpen}
+                onClose={() => setEditAdModalOpen(false)}
+            />
+            <ModalDeleteAd
+                isOpen={deleteAdModalOpen}
+                onClose={() => setDeleteAdModalOpen(false)}
+            />
+        </Flex>
+    );
 };
