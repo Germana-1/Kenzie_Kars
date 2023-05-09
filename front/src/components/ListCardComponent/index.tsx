6c0db8f3d934f497afc99fc395e6147830c8c89b
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2023-05-09 01:17:36 -0300

    feat: add cards paging front

--- front/src/components/ListCardComponent/index.tsx
+++ front/src/components/ListCardComponent/index.tsx
@@ -1,10 +1,11 @@
-import { Flex } from "@chakra-ui/react";
-import { useContext } from "react";
-
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { useContext, useState } from "react";
 import { CardComponent } from "../../components/CardComponent";
 import { AnnouncementContext } from "../../contexts/announcementContext";
 import { IAnnouncement } from "../../interfaces/announcement.interface";
 import { useParams } from "react-router-dom";
+import { Colors } from "../../styles/colors";
+import { StyledReactPaginate } from "./style";
 
 interface IListCardComponent {
   centered?: boolean;
@@ -19,35 +20,93 @@ export const ListCardComponent = ({
 }: IListCardComponent) => {
   const { id } = useParams();
   const { announcements } = useContext(AnnouncementContext);
-  let data = announcements;
+  const [pageNumber, setPageNumber] = useState(0);
+  const announcementsPerPage = 16;
 
+  let data = announcements;
   if (id) data = announcements.filter((el) => el.user?.id === id);
 
+  const pagesVisited = pageNumber * announcementsPerPage;
+  const pageCount = Math.ceil(data.length / announcementsPerPage);
+
+  const handlePageChange = (selected: number) => {
+    setPageNumber(selected);
+  };
+
   return (
-    <Flex
-      wrap={{ sm: "nowrap", md: "wrap" }}
-      overflowX={"auto"}
-      gap={"25px"}
-      maxW={centered ? "986.95px" : "100%"}
-    >
-      {filterActive
-        ? data.map(
-            (announce: IAnnouncement) =>
-              announce.isActive && (
+    <>
+      <Box>
+        <Flex
+          wrap={{ sm: "nowrap", md: "wrap" }}
+          overflowX={"auto"}
+          gap={"25px"}
+          maxW={centered ? "986.95px" : "100%"}
+        >
+          {filterActive
+            ? data
+              .slice(pagesVisited, pagesVisited + announcementsPerPage)
+              .map(
+                (announce: IAnnouncement) =>
+                  announce.isActive && (
+                    <CardComponent
+                      announce={announce}
+                      hideTag={hideTag}
+                      key={announce.id}
+                    />
+                  )
+              )
+            : data
+              .slice(pagesVisited, pagesVisited + announcementsPerPage)
+              .map((announce: IAnnouncement) => (
                 <CardComponent
                   announce={announce}
                   hideTag={hideTag}
                   key={announce.id}
                 />
+              ))}
+        </Flex>
+      </Box>
+      {data.length > announcementsPerPage && (
+        <Flex justifyContent={"center"}>
+          <StyledReactPaginate
+            previousLabel={
+              pagesVisited > 0 && (
+                <Button
+                  marginRight={50}
+                  bgColor={"transparent"}
+                  fontWeight={600}
+                  color={Colors.brand1}
+                  fontSize={24}
+                  size="sm">
+                  &lt; Anterior
+                </Button>
               )
-          )
-        : data.map((announce: IAnnouncement) => (
-            <CardComponent
-              announce={announce}
-              hideTag={hideTag}
-              key={announce.id}
-            />
-          ))}
-    </Flex>
+            }
+            nextLabel={
+              pageNumber !== pageCount - 1 ? (
+                <Button
+                  marginLeft={50}
+                  bgColor={"transparent"}
+                  fontWeight={600}
+                  color={Colors.brand1}
+                  fontSize={24}
+                  size="sm">
+                  Seguinte &gt;
+                </Button>
+              ) : null
+            }
+            breakLabel={"..."}
+            pageCount={pageCount}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={4}
+            onPageChange={({ selected }) => handlePageChange(selected)}
+            containerClassName={"pagination-container"}
+            pageClassName={"pagination-page"}
+            pageLinkClassName={"pagination-link"}
+            activeLinkClassName="pagination-active"
+          />
+        </Flex>
+      )}
+    </>
   );
 };
